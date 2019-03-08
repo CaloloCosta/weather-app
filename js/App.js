810fb3eb043d524c0cce205c918627ae9a31458a
Carlos Costa <carlosdaviddacosta@gmail.com>
2019-03-08 13:42:08 +0200

    Update App.js

--- js/App.js
+++ js/App.js
@@ -16,14 +16,27 @@ function requestInfo(city){
     ui.cleanScreen();
     ui.loader();
     r.getData(city).then(data => {
-      console.log(parseFloat(data.main.temp));
-      let t = `${celsius(data.main.temp)} ºC | ${data.main.temp} ºF`;
-      let min = `${celsius(data.main.temp_min)} ºC | ${data.main.temp_min} ºF`;
-      let max = `${celsius(data.main.temp_max)} ºC | ${data.main.temp_max} ºF`;
-      ui.mainCard(t,`${data.name}, ${data.mes.country}`,date(),data.main.humidity,min,max,data.temp[0].icon);
+      console.log(data);
+      let t = `${celsius(data.main.temp)} ºC | ${Math.floor(data.main.temp)} ºF`;
+      let min = `${celsius(data.main.temp_min)} ºC | ${Math.floor(data.main.temp_min,0)} ºF`;
+      let max = `${celsius(data.main.temp_max)} ºC | ${Math.floor(data.main.temp_max,0)} ºF`;
+      ui.mainCard(t,`${data.name}, ${data.mes.country}`,date(),data.main.humidity,min,max,data.temp[0].icon,data.temp[0].description);
+      r.forecast(city).then(
+        d =>{
+
+          for(let i = 7; i <= 21; i +=7){
+            let day = new Date(d[i].dt_txt).getDay();
+            let min = `${celsius(d[i].main.temp_min)} | ${Math.floor(d[i].main.temp_min)}`;
+            let max = `${celsius(d[i].main.temp_max)} | ${Math.floor(d[i].main.temp_max)}`;
+            ui.forecast(celsius(d[i].main.temp),Math.floor(d[i].main.temp),min,max,givenDay(day),d[i].weather[0].icon);
+
+          }
+        }
+      ).catch(e => console.log(e));
       ui.removeLoader();
     }).catch((error)=>{
-      ui.warning("cityNotFound")
+      ui.removeLoader();
+      ui.warning("cityNotFound");
     });
   }else{
     ui.warning("invalidCity");
@@ -34,24 +47,30 @@ function requestInfo(city){
 function currentLocation(){
   // check if geolocation is avalable
   if("geolocation" in navigator){
-    ui.loader();
     navigator.geolocation.getCurrentPosition(p =>{
+      ui.loader();
       r.getDataByLatLon(Math.floor(p.coords.latitude),Math.floor(p.coords.longitude))
       .then(data => {
-        console.log(parseFloat(data.main.temp));
-        let t = `${celsius(data.main.temp)} ºC | ${data.main.temp} ºF`;
+        let t = `${celsius(data.main.temp)} ºC | ${Math.floor(data.main.temp)} ºF`;
         let min = `${celsius(data.main.temp_min)} ºC | ${data.main.temp_min} ºF`;
         let max = `${celsius(data.main.temp_max)} ºC | ${data.main.temp_max} ºF`;
-        ui.mainCard(t,`${data.name}, ${data.mes.country}`,date(),data.main.humidity,min,max,data.temp[0].icon);
+        ui.mainCard(t,`${data.name}, ${data.mes.country}`,date(),data.main.humidity,min,max,data.temp[0].icon,data.temp[0].description);
         ui.removeLoader();
+        r.forecastLatLon(Math.floor(p.coords.latitude),Math.floor(p.coords.longitude))
+        .then(d =>{
+          for(let i = 7; i <= 21; i +=7){
+            let day = new Date(d[i].dt_txt).getDay();
+            let min = `${celsius(d[i].main.temp_min)} | ${Math.floor(d[i].main.temp_min)}`;
+            let max = `${celsius(d[i].main.temp_max)} | ${Math.floor(d[i].main.temp_max)}`;
+            ui.forecast(celsius(d[i].main.temp),Math.floor(d[i].main.temp),min,max,givenDay(day),d[i].weather[0].icon);
+          }
+        });
       }).catch((error)=>{
         ui.warning("cityNotFound");
       });
+    }, e =>{
+      ui.turnGeoLocation();
     });
-
-  }
-  else{
-    ui.turnGeoLocation();
   }
 
 }
@@ -69,3 +88,9 @@ function currentLocation(){
     const date = `${days[d.getDay()]} | ${m[d.getMonth()]} ${d.getDate()}`;
     return date;
   }
+// given days
+function givenDay(d){
+  const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Satarday'];
+  return days[d];
+
+}
