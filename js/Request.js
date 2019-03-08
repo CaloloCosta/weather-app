d369311d99bd45c9b4658792545a60f6ac285584
Carlos Costa <carlosdaviddacosta@gmail.com>
2019-03-08 13:42:59 +0200

    Update Request.js

--- js/Request.js
+++ js/Request.js
@@ -2,6 +2,7 @@ class Request{
   constructor(){
      this.key = "d850d2105f7747e173559bf224f7c2f8";
   }
+  // get data by city name
   async getData(city){
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${this.key}&units=imperial`;
     const response = await fetch(url);
@@ -10,7 +11,8 @@ class Request{
       main: data.main,
       temp: data.weather,
       mes: data.sys,
-      name: data.name
+      name: data.name,
+      weather: data.weather
     };
     return obj;
   }
@@ -27,7 +29,22 @@ class Request{
       name: data.name
     };
     return obj;
+  }
 
+  // forecast data by city name
+  async forecast(city){
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&APPID=${this.key}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.list;
+  }
+
+  // forecast data by city ID
+  async forecastLatLon(lat,lon){
+    const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&APPID=${this.key}&units=imperial`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.list;
   }
 
 }
